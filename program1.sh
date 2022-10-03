42a626be7d267e725ccc10cc1660e3eb43abbbb6
mittal7096 <101448589+mittal7096@users.noreply.github.com>
2022-10-03 10:37:57 +0000

    Add files via upload

--- program1.sh
+++ program1.sh
@@ -4,5 +4,10 @@ echo "enter second no:-"
 read n2
 sum1=`expr $n1 + $n2`
 mul1=`expr $n1 \* $n2`
+sub1=`expr $n1 - $n2`
+rem1=`expr $n1 % $n2`
+
 echo "sum of two enter number is :- $sum1" 
-echo "sum of two enter number is :- $mul1" 
+echo "multiplication of two enter number is :- $mul1" 
+echo "subtraction of two enter number is :- $sub1" 
+echo "reminder of two enter number is :- $rem1" 
