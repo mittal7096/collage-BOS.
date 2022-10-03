61008cf9e0c10b7a26cfa6a71a6b129e5397160e
mittal7096 <101448589+mittal7096@users.noreply.github.com>
2022-10-03 10:28:16 +0000

    Add files via upload

---
+++ program1.sh
@@ -0,0 +1,8 @@
+echo "enter first no:-"
+read n1
+echo "enter second no:-"
+read n2
+sum1=`expr $n1 + $n2`
+mul1=`expr $n1 \* $n2`
+echo "sum of two enter number is :- $sum1" 
+echo "sum of two enter number is :- $mul1" 
